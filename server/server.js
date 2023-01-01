ba923c321553aec29ed9cb4b3ca42606fd311b06
oyoops <121578862+oyoops@users.noreply.github.com>
2023-01-01 18:00:03 -0500

    server documentation

--- server/server.js
+++ server/server.js
@@ -3,32 +3,36 @@ import * as dotenv from 'dotenv'
 import cors from 'cors'
 import { Configuration, OpenAIApi } from 'openai'
 
+// load .env vars
 dotenv.config()
 
-// define the configuration for an OpenAI instance
+// define a configuration for an OpenAI instance
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
-// create an instance of OpenAI
+// create the OpenAI instance
 const openai = new OpenAIApi(configuration);
 
+// create the Express server
 const app = express()
 app.use(cors())
 app.use(express.json())
 
-// (dummy route)
+// (dummy GET route)
 app.get('/', async (req, res) => {
   res.status(200).send({
     message: 'Hello from oyoops AI!'
   })
 })
 
-// send user's input as a payload to OpenAI API
+// (dummy POST route)
 app.post('/', async (req, res) => {
+  // send user's input as a payload to OpenAI API
   try {
+    // get user's prompt (immediate)
     const prompt = req.body.prompt;
-
+    // get AI's response (***async***)
     const response = await openai.createCompletion({
       model: "text-davinci-003",
       prompt: `${prompt}`,
@@ -38,16 +42,17 @@ app.post('/', async (req, res) => {
       frequency_penalty: 0.75, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
       presence_penalty: 0.25, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
     });
-
+    // send back the AI's message as the response to client
     res.status(200).send({
       bot: response.data.choices[0].text
     });
 
   } catch (error) {
+    // if error, send back an error as the response to client
     console.error(error)
     res.status(500).send(error || 'UH-OH!! Something went wrong. Better luck next time...');
   }
 })
 
-// start listening!
+// make server begin listening for GET and POST requests
 app.listen(5000, () => console.log('oyoops AI server started on http://localhost:5000'))
