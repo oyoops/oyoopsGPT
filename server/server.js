51da1e17ba48eff0289114b9f396d609e6eff236
oyoops <121578862+oyoops@users.noreply.github.com>
2023-01-02 21:30:37 -0500

    undo all logging

--- server/server.js
+++ server/server.js
@@ -1,4 +1,3 @@
-export default import('dd-trace').init({ appsec: true });
 import express from 'express'
 import * as dotenv from 'dotenv'
 import cors from 'cors'
@@ -7,20 +6,6 @@ import Twit from 'twit'
 import path from 'path'
 import axios from 'axios'
 import useragent from 'express-useragent'
-import { createLogger, format, transports } from 'winston';
-
-// superlogger (DataDog -- ***** Free Trial Expires 1/16/23 *****)
-const logger = createLogger({
-  level: 'info',
-  exitOnError: false,
-  format: format.json(),
-  transports: [
-    new transports.File({ filename: `${appRoot}/logs/loggy.log` }),
-  ],
-});
-logger.log('info', 'Hello simple log!');
-logger.info('Hello log with metas',{color: 'blue' });
-//export default logger;
 
 const DEBUG_MODE = false;
 
