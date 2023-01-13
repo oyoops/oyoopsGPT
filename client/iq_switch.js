754bdfca5112d554c5ded94f0fc4656ddb91947a
oyoops <121578862+oyoops@users.noreply.github.com>
2023-01-13 17:57:01 -0500

    Second test of Switch IQ

--- client/iq_switch.js
+++ client/iq_switch.js
@@ -1,9 +1,11 @@
 function updateSwitchValue(elem) {
+    console.log("Called")
     var switchValue = elem.value;
     if (elem.checked) {
       elem.value = "150";
     } else {
       elem.value = "65";
     }
+    console.log("Called -- " + elem.value)
     document.getElementById("switch").value = switchValue;
 };
