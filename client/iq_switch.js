a5e5c74a7649edeec7b40d42340abcd6bc094805
oyoops <121578862+oyoops@users.noreply.github.com>
2023-01-21 14:23:54 -0500

    Revert "Second test of Switch IQ"
    This reverts commit 754bdfca5112d554c5ded94f0fc4656ddb91947a.

--- client/iq_switch.js
+++ client/iq_switch.js
@@ -1,11 +1,9 @@
 function updateSwitchValue(elem) {
-    console.log("Called")
     var switchValue = elem.value;
     if (elem.checked) {
       elem.value = "150";
     } else {
       elem.value = "65";
     }
-    console.log("Called -- " + elem.value)
     document.getElementById("switch").value = switchValue;
 };
